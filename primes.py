09c37628c3a9983158116ca2660aab70d7138c65
Fernanda Hernandez <54731581+ferhf@users.noreply.github.com>
2020-07-06 21:43:47 -0500

    Creación de clase prime_class, con método is_prime, que verifica si un número es primo.

--- primes.py
+++ primes.py
@@ -1,16 +1,43 @@
 """
-Your module documentation here
+Modulo para verificar si un número es primo.
 """
 
-
-class PrimeClass(object):
+class prime_class(object):
     """
-    Your class documentation here
+    Clase que verifica si un número es primo o no, retorna boolean.
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        Método que recibe número entero y verifica si es número primo o no, retorna boolean.
+        Se verifica revisando el residuo de todas las divisiones de num_int entre cada número
+        desde 2 hasta num_int, si es que alguna de esas divisiones tiene como residuo cero, 
+        significa que no es primo, si termina el ciclo y nunca obtuvo una división con residuo 
+        cero (división exacta) entonces num_int es primo.
+        Se revisa que sea mayor a cero y en el caso de que sea 1, por defecto manda False.
+        """        
+        es_primo = True
+
+        if num_int > 1:
+
+            cont = 2
+
+            while cont != num_int:
+
+                if num_int % cont == 0:
+                    es_primo = False
+                    break
+                
+                cont += 1
+
+            # your primes code here
+            return es_primo
+        elif num_int == 1:
+            es_primo = False
+        else:
+            print('El número no es positivo mayor a cero.')
+            es_primo = False
+
+
+
+
