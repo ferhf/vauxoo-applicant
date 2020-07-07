6a83305d8425ed6ee8fe1d61c68647266aeb9082
Fernanda Hernandez <54731581+ferhf@users.noreply.github.com>
2020-07-06 21:55:39 -0500

    Se agrega clase calculator_class, se agrega método sum y código necesario para su funcionamiento.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,24 @@
 """
-Your module documentation here
+Módulo para realizar cálculos de suma.
 """
 
 
-class CalculatorClass(object):
+class calculator_class(object):
     """
-    Your class documentation here
+    Clase para realizar cálculos básicos.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Método para realizar suma. Se recibe una lista de números.
+        Se verifica que el parámetro recibido sea una lista, 
+        si es así, se realiza la suma de cada de uno de los elementos.  
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        suma = 0
+
+        if type(num_list) is list:
+            
+            for num in num_list:
+                suma += num
+                    
+        return suma
